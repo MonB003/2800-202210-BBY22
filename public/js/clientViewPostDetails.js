3fa438f97e255543a5fdd81c7e6d41d858548135
krispyrendang <taswinder.s@gmail.com>
2022-05-10 10:25:39 -0700

    created the html/js files for viewing an item post in detail.

---
+++ public/js/clientViewPostDetails.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// Updates a user's data in the database
+async function editpost(postID) {
+    let firstName = "yes";
+    let lastName = "no";
+
+    // Store user's data that was filled into the text fields on the page
+    const dataSent = {
+        firstName,
+        lastName,
+        postID
+    }
+
+    // Additional details needed when sending data to server side
+    const postDetails = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dataSent)
+    }
+
+    // Get response from server side post request called update-user-data
+    const postResponse = await fetch('/toeditpost', postDetails);
+    const jsonData = await postResponse.json();
+    if (jsonData.status == "Success") {
+        window.location.replace("/editpost");
+    }
+};
