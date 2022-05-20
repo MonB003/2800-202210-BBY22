d0a04809dbcb9c4d1d8636b984030dd52373189a
Alan Fung <afung63@my.bcit.ca>
2022-05-19 22:42:33 -0700

    added even listener for navbar on the view user profile page

--- public/js/clientProfile.js
+++ public/js/clientProfile.js
@@ -1,11 +1,33 @@
 "use strict";
 
-// Directs to main page when home button is clicked
+// Redirects to main page
 document.querySelector("#home").addEventListener("click", function (e) {
     window.location.replace("/main");
 });
+document.querySelector("#home2").addEventListener("click", function (e) {
+    window.location.replace("/main");
+});
 
-// Directs to client listing page
-document.querySelector("#mylistings").addEventListener("click", function (e) {
+//redirects to message page
+document.querySelector("#messages").addEventListener("click", function (e) {
+    window.location.replace("/message");
+});
+document.querySelector("#messages2").addEventListener("click", function (e) {
+    window.location.replace("/message");
+});
+
+// When my listings button is clicked, redirect to myListings page
+document.querySelector("#listings").addEventListener("click", function (e) {
     window.location.replace("/mylistings");
+});
+document.querySelector("#listings2").addEventListener("click", function (e) {
+    window.location.replace("/mylistings");
+});
+
+// When profile button is clicked, redirect to profile page
+document.querySelector("#profile").addEventListener("click", function (e) {
+    window.location.replace("/profile");
+});
+document.querySelector("#profile2").addEventListener("click", function (e) {
+    window.location.replace("/profile");
 });
